95af8b52ad80bb0ac912755599fe49d60fe204f1
wilschok <william.schokkele@howest.be>
2019-09-17 15:07:40 +0200

    Add new models

---
+++ BookService.WebAPI/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookService.WebAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
---
+++ BookService.WebAPI/Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace BookService.WebAPI.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+    }
+}
