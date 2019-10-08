ec54b5b865ccfb686dbe315d9adc52aa39428968
wilschok <william.schokkele@howest.be>
2019-10-08 13:26:01 +0200

    Add Automapper configuration, Add Mappingrepository

---
+++ BookService.WebAPI/Repositories/MappingRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookService.WebAPI.Models;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class MappingRepository<T>: Repository<T> where T: EntityBase
+    {
+        protected readonly IMapper mapper;
+        public MappingRepository(BookServiceContext context, IMapper mapper): base (context)
+        {
+            this.mapper = mapper;
+        }
+    }
+}
--- BookService.WebAPI/Startup.cs
+++ BookService.WebAPI/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookService.WebAPI.Models;
 using BookService.WebAPI.Repositories;
+using BookService.WebAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -37,7 +38,13 @@ namespace BookService.WebAPI
             services.AddScoped<ReaderRepository>();
             services.AddScoped<RatingRepository>();
 
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+           {
+               cfg.AddProfile(new AutoMapperProfileConfiguration());
+           });
 
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
 
         }
 
