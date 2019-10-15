6666f98ff2d7acc4331ce7a32a7e8667b99ab9df
wilschok <william.schokkele@howest.be>
2019-10-15 13:46:46 +0200

    CRUD Publishers

--- BookService.MVC/Controllers/PublishersController.cs
+++ BookService.MVC/Controllers/PublishersController.cs
@@ -24,9 +24,48 @@ namespace BookService.MVC.Controllers
         public IActionResult Detail(int id)
         {
             string uri = $"{baseuri}/{id}";
-
+            ViewBag.Mode = "Detail";
             return View(WebApiHelper.GetApiResult<Publisher>(uri));
 
+        }
+
+        public IActionResult Edit(int id) { 
+            string uri = $"{baseuri}/{id}"; 
+            ViewBag.Mode = "Edit"; 
+            return View("Detail", WebApiHelper.GetApiResult<Publisher>(uri)); 
+        }
+
+        public IActionResult Insert()
+        {
+            ViewBag.Mode = "Edit";
+            return View("Detail", new Publisher());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Publisher publisher)
+        {
+            if (publisher.Id != 0)
+            {
+                // update
+                string uri = $"{baseuri}/{publisher.Id}";
+                publisher = await WebApiHelper.PutCallAPI<Publisher, Publisher>(uri, publisher);
+            }
+            else
+            {
+                // insert
+                string uri = $"{baseuri}";
+                publisher = await WebApiHelper.PostCallAPI<Publisher, Publisher>(uri, publisher);
+            }
+
+            ViewBag.Mode = "Detail";
+            return View("Detail", publisher);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            string uri = $"{baseuri}/{id}";
+            Publisher publisher = await WebApiHelper.DelCallAPI<Publisher>(uri);
+            return RedirectToAction("Index");
         }
     }
 }
